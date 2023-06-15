24ea836c017fbe03b0f97638f769761aab348834
danielatego <danielatego75@gmail.com>
2023-06-15 15:29:33 +0300

    beautiful code

--- boxchallenge/src/App.js
+++ boxchallenge/src/App.js
@@ -5,23 +5,23 @@ import "./style.css";
 function App(props) {
   const[box, setBox]= React.useState(boxes)
   function toggle(id){
-    const v = id-1;
+    //const v = id-1;
     setBox(boxi=>{
-      var newBox= [];
-      for (var i=0;i<boxi.length;i++){
-        if (i===v){
-          const ntewBox={id:id,on:!boxi[i].on}
-          newBox.push(ntewBox)
-        }
-        else{
-          newBox.push(boxi[i]);
-        }
+      return boxi.map(square=>(
+        square.id === id ? {...square,on:!square.on}:square
+      ))
+      // var newBox= [];
+      // for (var i=0;i<boxi.length;i++){
+      //   if (i===v){
+      //     const ntewBox={id:id,on:!boxi[i].on}
+      //     newBox.push(ntewBox)
+      //   }
+      //   else{
+      //     newBox.push(boxi[i]);
+      //   }
         
-      }
-      console.log(newBox)
-      return newBox;
-      
-      
+      // }
+      // return newBox;
     }
     )
     
