2302c5fff9ecde2b427401770fc5efb794f7f75c
danielatego <danielatego75@gmail.com>
2023-06-15 15:13:16 +0300

    changing a root property from the child

--- boxchallenge/src/App.js
+++ boxchallenge/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
+import boxes from './boxes'
+import Box from "./component/box";
+import "./style.css";
+function App(props) {
+  const[box, setBox]= React.useState(boxes)
+  function toggle(id){
+    const v = id-1;
+    setBox(boxi=>{
+      var newBox= [];
+      for (var i=0;i<boxi.length;i++){
+        if (i===v){
+          const ntewBox={id:id,on:!boxi[i].on}
+          newBox.push(ntewBox)
+        }
+        else{
+          newBox.push(boxi[i]);
+        }
+        
+      }
+      console.log(newBox)
+      return newBox;
+      
+      
+    }
+    )
+    
+  }
 
-function App() {
+  const boxesi = box.map(one=>(
+  <Box 
+    key={one.id}
+    id={one.id}
+    on={one.on}
+    fn = {toggle}
+  />
+    ))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main >
+      <h1>Boxes will go here</h1>
+      {boxesi}
+    </main>
   );
 }
 
---
+++ boxchallenge/src/component/box.js
@@ -0,0 +1,16 @@
+import React from "react";
+function Box(props){
+   
+    
+    
+    const styles ={
+      backgroundColor:props.on ? "#000000":"#FFFFFF",
+      color:"green"
+    }
+    return(
+        <div onClick={()=>props.fn(props.id)} style= {styles} className='box'key={props.id}>
+             {props.id}
+        </div>
+    )
+    }
+    export default Box;
