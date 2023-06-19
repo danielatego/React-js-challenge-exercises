513bace96f3ed6e8a153e8b03c4b2799d7335a1b
danielatego <danielatego75@gmail.com>
2023-06-19 19:50:46 +0300

    form inputs

--- forms/forms/src/App.js
+++ forms/forms/src/App.js
@@ -4,11 +4,15 @@ function App() {
   const [Registration,setFirstName]= React.useState({
     firstName:'',
     secondName:'',
-    email:''
+    email:'',
+    comment:'',
+    isFriendly: true
+
   })
   console.log(Registration)
   function handleChange(event){
-    setFirstName({...Registration,[event.target.name]:event.target.value})
+    const{name,value,type,checked}=event.target
+    setFirstName({...Registration,[name]:type === 'checkbox'?checked:value})
   }
   
   return (
@@ -18,19 +22,63 @@ function App() {
         placeholder='First Name'
         onChange={handleChange}
         name="firstName"
+        value={Registration.firstName}
       />
        <input 
         type='text'
         placeholder='Last Name'
         onChange={handleChange}
         name="secondName"
+        value={Registration.secondName}
+
       />
       <input 
         type='text'
         placeholder='email'
         onChange={handleChange}
         name="email"
+        value={Registration.email}
+
       />
+      <textarea 
+      name="comment" 
+      placeholder="comment"
+      onChange={handleChange}
+      cols="30" 
+      rows="10"
+      value={Registration.comment}
+      />
+      <br />
+      <br />
+      <input 
+        type="checkbox"
+        name="isFriendly"
+        checked={Registration.isFriendly}
+        onChange={handleChange}
+      />
+      <label htmlFor="isFriendly">Are you friendly</label>
+      <br />
+      <br />
+      <fieldset>
+        <legend> Current employment status</legend>
+        <input 
+          type="radio"
+          id="Unemployed"
+        />
+        <label htmlFor="Unemployed">Unemployed</label>
+        <br />
+        <input 
+          type="radio"
+          id="Part-time"
+        />
+        <label htmlFor="Part-time">Part-time</label>
+        <br />
+        <input 
+          type="radio"
+          id="Full-time"
+        />
+        <label htmlFor="Full-time">Full-time</label>
+      </fieldset>
     </form>
   );
 }
