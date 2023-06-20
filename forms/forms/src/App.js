e52909c88b84480965c863bf20843aaf7527271a
danielatego <danielatego75@gmail.com>
2023-06-20 16:59:28 +0300

    complete react form

--- forms/forms/src/App.js
+++ forms/forms/src/App.js
@@ -6,17 +6,21 @@ function App() {
     secondName:'',
     email:'',
     comment:'',
-    isFriendly: true
+    isFriendly: true,
+    employment:'',
+    favColor:''
 
   })
-  console.log(Registration)
   function handleChange(event){
     const{name,value,type,checked}=event.target
     setFirstName({...Registration,[name]:type === 'checkbox'?checked:value})
   }
-  
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(Registration)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input 
         type='text'
         placeholder='First Name'
@@ -24,22 +28,23 @@ function App() {
         name="firstName"
         value={Registration.firstName}
       />
+      <br />
        <input 
         type='text'
         placeholder='Last Name'
         onChange={handleChange}
         name="secondName"
         value={Registration.secondName}
-
       />
+      <br />
       <input 
         type='text'
         placeholder='email'
         onChange={handleChange}
         name="email"
         value={Registration.email}
-
       />
+      <br />
       <textarea 
       name="comment" 
       placeholder="comment"
@@ -52,6 +57,7 @@ function App() {
       <br />
       <input 
         type="checkbox"
+        id="isFriendly"
         name="isFriendly"
         checked={Registration.isFriendly}
         onChange={handleChange}
@@ -64,21 +70,55 @@ function App() {
         <input 
           type="radio"
           id="Unemployed"
-        />
+          name="employment" 
+          checked= {Registration.employment==='unemployed'}
+          value='unemployed' 
+          onChange={handleChange}
+       />
         <label htmlFor="Unemployed">Unemployed</label>
         <br />
         <input 
           type="radio"
           id="Part-time"
+          name="employment" 
+          checked={Registration.employment==='part-time'}      
+          value='part-time' 
+          onChange={handleChange}
         />
         <label htmlFor="Part-time">Part-time</label>
         <br />
         <input 
           type="radio"
           id="Full-time"
+          name="employment" 
+          checked={Registration.employment==='full-time'} 
+          value='full-time'
+          onChange={handleChange}
         />
         <label htmlFor="Full-time">Full-time</label>
       </fieldset>
+      <br />
+      <label htmlFor="favColor">What is your favourite color?</label>
+      <br />
+      <select 
+        id="favColor"
+        value={Registration.favColor}
+        onChange={handleChange}
+        name="favColor"
+      >
+        <option value="">-- Choose --</option>
+        <option value="red">Red</option>
+        <option value="orange">Orange</option>
+        <option value="yellow">Yellow</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+        <option value="indigo">Indigo</option>
+        <option value="violet">Violet</option>
+      </select>
+      <br />
+      <br />
+      {/* <input type="submit" value='submit' /> */}
+      <button>Submit</button>
     </form>
   );
 }
